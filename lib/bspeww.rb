265de0e660cc69eea9514d7e2691e008b589058e
Elliot Wesoff <ewesoff@gmail.com>
2023-07-09 13:36:58 -0700

    add comments to the gnar recursion method.

--- lib/bspeww.rb
+++ lib/bspeww.rb
@@ -42,10 +42,19 @@ class Bspeww
     @desktop_window_names[desktop['name']] = traverse(desktop['root'])
   end
 
+  # recursively traverse the child nodes of the bspwm node tree.
   def traverse(client)
-    return '' if client.nil?
-    return client['client']['className'] unless client['client'].nil?
+    return '' if client.nil? # should only be when root is null - no open windows
 
+    # it appears that bspwm maintains a *full* binary tree. since you probs
+    # won't remember in the future, that means that a node has exactly 0 or
+    # 2 children. the 'client' property contains the information regarding
+    # a window, so if 'client' is not null, then it must not have any children
+    # and we can stop recursing.
+    return client['client']['instanceName'] unless client['client'].nil?
+
+    # client is null, so there must be two children associated with this node.
+    # traverse both of them and combine the window names into the result string.
     [traverse(client['firstChild']), traverse(client['secondChild'])].join('; ')
   end
 end
